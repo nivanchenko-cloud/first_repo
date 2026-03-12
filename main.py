f51fe95d0d254e672336d30389839e299584ff65
nivanchenko-cloud <n.ivanchenko992@gmail.com>
2026-03-12 15:05:33 +0100

    first-commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print ("Let's play tic tac toe")
+sign = "X"
+print ("X")
+SIGNS = {1: "X", 2: "O"}
+MENU = """Hello! Welcome to Tic Tac Toe game!
+Rules: X and O takes turns marking the spaces in a 3*3 grid"""
+print (MENU)
+player = "X"
+PLAYER_WON_MESSAGE = f"Player {player}, you won!"
+print (PLAYER_WON_MESSAGE)
+
